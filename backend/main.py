c85176a6bf1907c39c776a0d5f8debd31cf420ab
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-09-22 17:59:17 +0530

    frontend url updated

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ CORS(app, resources={
     r"/*": {
         "origins": [
             "http://localhost:5173",
-            "https://hirekruit.vercel.app"
+            "https://hirekruit.com"
         ]
     }
 })
