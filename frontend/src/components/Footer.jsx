6a5583829b104f6a150754cc95d2231e3f438ec4
RahulKumar2340029 <143934828+RahulKumar2340029@users.noreply.github.com>
2025-09-25 19:48:27 +0530

    footer corrected

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <div>
 
 
-            <footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+            <footer class="bg-white rounded-lg h-[50px]">
                 <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between pl-0">
-                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" class="hover:underline">HiRekruit</a>. All Rights Reserved.
+                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://hirekruit.com/" class="hover:underline">HiRekruit</a>. All Rights Reserved.
                     </span>
                     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                         <li>
