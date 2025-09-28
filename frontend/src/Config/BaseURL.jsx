7471f627a9b8fb54d2a1809a83ca0b665e4ce8a5
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-09-28 19:21:17 +0530

    baseurl updated

--- frontend/src/Config/BaseURL.jsx
+++ frontend/src/Config/BaseURL.jsx
@@ -1,9 +1,9 @@
 const config = {
   // Uncomment the one you want to use
-  BASE_URL: "http://localhost:5000", // Local development URL
+  // BASE_URL: "http://localhost:5000", // Local development URL
 
   // For deployment
-  // BASE_URL: "https://hiremate-dde0.onrender.com", // Deployed production URL
+  BASE_URL: "https://hiremate-dde0.onrender.com", // Deployed production URL
 };
 
 export default config;
