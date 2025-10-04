02678c0a49348453accf5180e0771aa76a2437e4
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-10-04 11:52:40 +0530

    gtm tagmanager improted in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/HiRekruit.png";
+import TagManager from "react-gtm-module";
 import {
   Menu,
   X,
